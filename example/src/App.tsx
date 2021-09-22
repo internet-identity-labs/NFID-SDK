2fdea43c575cfe100e7426c391afe1576ecd1277
Philipp Litzenberger <philipp@bendcircular.com>
2021-09-22 11:47:41 +0200

    chore(ii auth): repo init

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-ic-ii-auth'
+import 'react-ic-ii-auth/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
