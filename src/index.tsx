8d2dc0a04d7f01ec631765036073c1d238fc76fc
Philipp Litzenberger <philipp@bendcircular.com>
2021-09-23 17:12:54 +0200

    fix(useiciiauth): set isAuthenticated state in success handler
    fix #11

--- src/index.tsx
+++ src/index.tsx
@@ -58,6 +58,7 @@ const useICIIAuth = ({
   }, [authClient, setAuthStatus, identity])
 
   const handleOnSuccess = React.useCallback((authClient) => {
+    setIsAuthenticated(true)
     onSuccess && onSuccess(authClient.getIdentity())
   }, [])
 
