814093b83d766dcab95e963b5364afe8bc86e757
Philipp Litzenberger <philipp@bendcircular.com>
2021-09-23 16:51:37 +0200

    chore(example): remove broken example
    @dfinity/agent requires unsupported typescript version

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,7 +1,27 @@
-import { InternetIdentityProvider } from '.'
+import React from 'react'
+import { render, waitFor } from '@testing-library/react'
+import { InternetIdentityProvider, useInternetIdentity } from '.'
 
-describe('ExampleComponent', () => {
-  it('is truthy', () => {
-    expect(InternetIdentityProvider).toBeTruthy()
+const AuthButton = () => {
+  const { authenticate, signout, isAuthenticated } = useInternetIdentity()
+  return (
+    <button onClick={isAuthenticated ? signout : authenticate}>
+      {isAuthenticated ? 'Log out' : 'Log in'}
+    </button>
+  )
+}
+
+const Sample = () => {
+  return (
+    <InternetIdentityProvider>
+      <AuthButton />
+    </InternetIdentityProvider>
+  )
+}
+
+it('renders without crashing', async () => {
+  const { getByText } = render(<Sample />)
+  await waitFor(() => {
+    expect(getByText('Log in')).toBeDefined()
   })
 })
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,8 @@
-// jest.setup.ts
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom/extend-expect'
 import '@testing-library/jest-dom'
 
 // https://stackoverflow.com/questions/68468203/why-am-i-getting-textencoder-is-not-definedin-jest
