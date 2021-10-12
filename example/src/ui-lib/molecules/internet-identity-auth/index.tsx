306184db749c420b9b2a9b2bc9234f207de11702
Philipp Litzenberger <philipp@bendcircular.com>
2021-10-12 19:43:30 +0200

    feat(iframe auht): add auth iframe proxy component

--- example/src/ui-lib/molecules/internet-identity-auth/index.tsx
+++ example/src/ui-lib/molecules/internet-identity-auth/index.tsx
@@ -1,31 +1,27 @@
-import clsx from "clsx";
-import React from "react";
-import { Loader } from "src/ui-lib/atoms/loader";
+import clsx from 'clsx'
+import React from 'react'
+import { AuthIframe } from '@bendcircular/react-ic-ii-auth'
+import { Loader } from 'src/ui-lib/atoms/loader'
 
 interface AuthContentProps {
-  internetIdentityProvider: string;
+  internetIdentityProvider: string
 }
 
 export const InternetIdentityIframe: React.FC<AuthContentProps> = ({
-  internetIdentityProvider,
+  internetIdentityProvider
 }) => {
-  const [isLoading, loading] = React.useState(true);
+  const [isLoading, loading] = React.useState(true)
   return (
-    <div className={clsx(["h-[90%] md:h-full md:overflow-scroll"])}>
-      <div className={clsx(["h-full w-full", isLoading ? "block" : "hidden"])}>
+    <div className={clsx(['h-[90%] md:h-full md:overflow-scroll'])}>
+      <div className={clsx(['h-full w-full', isLoading ? 'block' : 'hidden'])}>
         <Loader />
       </div>
-      <div className={clsx(["h-full pb-4"])}>
-        <iframe
-          title="idpWindow"
-          name="idpWindow"
+      <div className={clsx(['h-full pb-4'])}>
+        <AuthIframe
           src={internetIdentityProvider}
-          width="100%"
-          height="100%"
-          allow="publickey-credentials-get"
           onLoad={() => loading(false)}
         />
       </div>
     </div>
-  );
-};
+  )
+}
