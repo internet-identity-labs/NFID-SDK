a816eb0bc39109a6014b3784f76b0b5fff29b457
Philipp Litzenberger <philipp@bendcircular.com>
2022-03-28 10:50:55 +0200

    chore(nfid-sdk-react): configure storybook and tailwind

--- packages/nfid-sdk-react/src/atoms/button/index.stories.test.tsx
+++ packages/nfid-sdk-react/src/atoms/button/index.stories.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Default as Thing } from './Button.stories';
+import { Default as Thing } from './index.stories';
 
 describe('Thing', () => {
   it('renders without crashing', () => {
--- packages/nfid-sdk-react/src/atoms/button/index.stories.tsx
+++ packages/nfid-sdk-react/src/atoms/button/index.stories.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import { Button, ButtonProps } from '../src';
+import { Button, ButtonProps } from '../..';
 
 const meta: Meta = {
-  title: 'Welcome',
+  title: 'Atoms/Button',
   component: Button,
   argTypes: {
     children: {
@@ -19,10 +19,12 @@ const meta: Meta = {
 
 export default meta;
 
-const Template: Story<ButtonProps> = args => <Button {...args} />;
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
 
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
 export const Default = Template.bind({});
 
-Default.args = {};
+Default.args = {
+  children: 'Button',
+};
