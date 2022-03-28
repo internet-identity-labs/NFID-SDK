03e2e9ade4408af080d27492d097f618df49dfe0
Philipp Litzenberger <philipp@bendcircular.com>
2022-03-28 08:14:00 +0200

    chore(nfid-sdk-react): use tsdx and integrate tailwindcss

--- packages/nfid-sdk-react/src/index.test.tsx
+++ packages/nfid-sdk-react/src/index.test.tsx
@@ -1,7 +1,7 @@
-import { ExampleComponent } from '.'
+import { Thing } from '.';
 
 describe('ExampleComponent', () => {
   it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
-  })
-})
+    expect(Thing).toBeTruthy();
+  });
+});
