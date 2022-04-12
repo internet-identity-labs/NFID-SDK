8b415b04b0fd563485aea85a34beb3d7ce4cefcf
Mert <mertpolat.98@hotmail.com>
2022-04-12 23:49:34 +0200

    feat: add NFID theme to storybook

---
+++ packages/nfid-sdk-react/.storybook/theme.js
@@ -0,0 +1,13 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  appBg: '#fff',
+  appContentBg: '#fff',
+  colorSecondary: '#000',
+  barTextColor: 'gray',
+  brandTitle: 'NFID SDK',
+  brandUrl: 'https://github.com/internet-identity-labs/NFID-SDK',
+  brandImage:
+    'https://user-images.githubusercontent.com/33829677/162224559-f38581ec-fbd7-4ab0-a99a-5bf5332811b3.png',
+});
