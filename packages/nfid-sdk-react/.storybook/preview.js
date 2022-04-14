3eb7b2d431f6b212cf9a13ec26fdbe5f41b6eb71
Philipp Litzenberger <philipp@bendcircular.com>
2022-04-14 10:59:51 +0200

    chore(nfid-sdk-react): add mobile viewports

--- packages/nfid-sdk-react/.storybook/preview.js
+++ packages/nfid-sdk-react/.storybook/preview.js
@@ -1,5 +1,10 @@
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 export const parameters = {
   // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
   actions: { argTypesRegex: '^on.*' },
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
 };
