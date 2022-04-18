a9032d61c0b453ec2822ea660042d39bca8da243
Pavlo Chorniy <pchorniy@proffiz.com>
2022-04-18 20:01:31 +0200

    added some box-shadows to theme.js

--- packages/nfid-ui-kit-core/src/theme.js
+++ packages/nfid-ui-kit-core/src/theme.js
@@ -66,7 +66,11 @@ const boxShadow = {
   black: "0 3px 10px rgba(59,62,67,0.4)",
   blueLight: "0 3px 10px rgba(32,121,255,0.4)",
   iframe: "0 3px 20px rgba(0,0,0,0.1)",
-  red: "0 0px 2px rgba(234,26,26,1)"
+  red: "0 0px 2px rgba(234,26,26,1)",
+  gray: "0 2px 20px -1px #0000001a,0 0px 4px -2px #0000001a",
+  lightGray: "0px 2px 10px rgba(15, 12, 13, 0.4)",
+  lightCyan: "0px 2px 10px rgba(29, 161, 242, 0.6)",
+  lightBlue: "0px 2px 10px rgba(88, 101, 242, 0.6)",
 };
 
 const blur = {
