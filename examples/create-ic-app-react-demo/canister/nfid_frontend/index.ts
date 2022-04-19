69180d2ca8074cf5761cd4c29314e46cba6b6e87
Philipp Litzenberger <philipp@bendcircular.com>
2022-04-19 08:57:50 +0200

    fix: assign globals by blocking script

---
+++ examples/create-ic-app-react-demo/canister/nfid_frontend/index.ts
@@ -0,0 +1,10 @@
+declare const FRONTEND_MODE: string
+declare const II_ENV: string
+declare const INTERNET_IDENTITY_CANISTER_ID: string
+declare const IC_HOST: string
+declare const PUB_SUB_CHANNEL_CANISTER_ID: string
+;(window as any).FRONTEND_MODE = FRONTEND_MODE
+;(window as any).II_ENV = II_ENV
+;(window as any).IC_HOST = IC_HOST
+;(window as any).INTERNET_IDENTITY_CANISTER_ID = INTERNET_IDENTITY_CANISTER_ID
+;(window as any).PUB_SUB_CHANNEL_CANISTER_ID = PUB_SUB_CHANNEL_CANISTER_ID
--- examples/create-ic-app-react-demo/canister/nfid_frontend/webpack.config.js
+++ examples/create-ic-app-react-demo/canister/nfid_frontend/webpack.config.js
@@ -1,16 +1,43 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const webpack = require("webpack")
-const cheerio = require("cheerio")
 const path = require("path")
-var fs = require("fs")
+const fs = require("fs")
 
+var canister_ids
+try {
+  canister_ids = JSON.parse(
+    fs
+      .readFileSync(
+        path.resolve(__dirname, "../../.dfx/local/canister_ids.json"),
+      )
+      .toString(),
+  )
+} catch (e) {
+  console.error("\n⚠️  Before starting the dev server run: dfx deploy\n\n")
+}
+
+const canisterEnv = Object.entries(canister_ids).reduce(
+  (acc, [key, val]) => ({
+    ...acc,
+    [`${key.toUpperCase()}_CANISTER_ID`]: JSON.stringify(val.local),
+  }),
+  {},
+)
 
 module.exports = {
   mode: "development",
-  entry: path.resolve(__dirname, "index.js"),
-  output: {
-    path: path.resolve(__dirname, "./build/"),
-    filename: "static/js/variables.js",
+  entry: path.resolve(__dirname, "index.ts"),
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
   },
   devServer: {
     port: 9090,
@@ -19,30 +46,16 @@ module.exports = {
     static: [path.resolve(__dirname, "build")],
   },
   plugins: [
-    new webpack.EnvironmentPlugin({
-      NODE_ENV: "development",
+    new webpack.DefinePlugin({
+      IC_HOST: JSON.stringify("http://localhost:8000"),
+      II_ENV: JSON.stringify("development"),
+      FRONTEND_MODE: JSON.stringify("development"),
+      ...canisterEnv,
     }),
     new HtmlWebpackPlugin({
       inject: true,
-      // template: path.resolve(__dirname, "build/index.html"),
-      templateContent: ({ htmlWebpackPlugin }) => {
-        const $ = cheerio.load(
-          fs.readFileSync(path.resolve(__dirname, "build/index.html"), "utf8"),
-        )
-        console.log(">> ", { html: $.html() })
-
-        $("head").append(`
-          <script>
-            var IC_HOST = "http://localhost:8000"
-            var II_ENV = "development"
-            var INTERNET_IDENTITY_CANISTER_ID = "sp3hj-caaaa-aaaaa-aaajq-cai"
-            var IDENTITY_MANAGER_CANISTER_ID = "zwmtd-uqaaa-aaaaa-aabwq-cai"
-            var PUB_SUB_CHANNEL_CANISTER_ID = "z7py7-cyaaa-aaaaa-aabxa-cai"
-            var FRONTEND_MODE = "development"
-          </script>
-        `)
-        return $.html()
-      },
+      scriptLoading: "blocking",
+      template: path.resolve(__dirname, "build/index.html"),
     }),
   ],
 }
