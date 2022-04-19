bad83f67ea0523fecc533d992c3fe5ba89d41890
Philipp Litzenberger <philipp@bendcircular.com>
2022-04-19 08:57:50 +0200

    feat: inject variables by cheerio

--- examples/create-ic-app-react-demo/canister/nfid_frontend/webpack.config.js
+++ examples/create-ic-app-react-demo/canister/nfid_frontend/webpack.config.js
@@ -1,6 +1,9 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const webpack = require("webpack")
+const cheerio = require("cheerio")
 const path = require("path")
+var fs = require("fs")
+
 
 module.exports = {
   mode: "development",
@@ -21,7 +24,25 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       inject: true,
-      template: path.resolve(__dirname, "build/index.html"),
+      // template: path.resolve(__dirname, "build/index.html"),
+      templateContent: ({ htmlWebpackPlugin }) => {
+        const $ = cheerio.load(
+          fs.readFileSync(path.resolve(__dirname, "build/index.html"), "utf8"),
+        )
+        console.log(">> ", { html: $.html() })
+
+        $("head").append(`
+          <script>
+            var IC_HOST = "http://localhost:8000"
+            var II_ENV = "development"
+            var INTERNET_IDENTITY_CANISTER_ID = "sp3hj-caaaa-aaaaa-aaajq-cai"
+            var IDENTITY_MANAGER_CANISTER_ID = "zwmtd-uqaaa-aaaaa-aabwq-cai"
+            var PUB_SUB_CHANNEL_CANISTER_ID = "z7py7-cyaaa-aaaaa-aabxa-cai"
+            var FRONTEND_MODE = "development"
+          </script>
+        `)
+        return $.html()
+      },
     }),
   ],
 }
