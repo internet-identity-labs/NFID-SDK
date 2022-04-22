9b2f17e4f64075ed7980c8f0adf86e82f5cab7a2
Philipp Litzenberger <philipp@bendcircular.com>
2022-04-22 14:41:35 +0200

    feat: move icons to top level atoms

--- packages/nfid-sdk-react/src/atoms/icons/index.stories.tsx
+++ packages/nfid-sdk-react/src/atoms/icons/index.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, Story } from '@storybook/react';
 import React from 'react';
+import { H1, H2 } from '../typography';
 import { ButtonChevronIcon } from './chevron';
 import { CopyIcon } from './copy';
 import { DesktopIcon } from './desktop';
@@ -26,21 +27,28 @@ const meta: Meta = {
 export default meta;
 
 const Icons: Story = (args) => {
+  const [activeIcon, setActiveIcon] = React.useState<string | null>(null);
   return (
-    <div className="flex gap-2 items-center">
-      <DesktopIcon {...args} />
-      <KeyIcon {...args} />
-      <LaptopIcon {...args} />
-      <MobileIcon {...args} />
-      <PencilIcon {...args} />
-      <TabletIcon {...args} />
-      <TrashIcon {...args} />
-      <ButtonChevronIcon {...args} />
-      <CopyIcon {...args} />
-      <DotsIcon {...args} />
-      <LogoutIcon {...args} />
-      <PlusIcon {...args} />
-      <RefreshIcon {...args} />
+    <div>
+      <div className="mb-2 font-bold">DeviceIcons:</div>
+      <div className="flex items-center gap-2">
+        <MobileIcon {...args} />
+        <TabletIcon {...args} />
+        <DesktopIcon {...args} />
+        <LaptopIcon {...args} />
+        <KeyIcon {...args} />
+      </div>
+      <div className="mt-4 mb-2 font-bold">Controls:</div>
+      <div className="flex items-center gap-2">
+        <PencilIcon {...args} />
+        <TrashIcon {...args} />
+        <ButtonChevronIcon {...args} />
+        <CopyIcon {...args} />
+        <DotsIcon {...args} />
+        <LogoutIcon {...args} />
+        <PlusIcon {...args} />
+        <RefreshIcon {...args} />
+      </div>
     </div>
   );
 };
