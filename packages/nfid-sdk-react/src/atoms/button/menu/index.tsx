7598c896d202c9b5c7b10f1e7504c85bc5753023
Pavlo Chorniy <pchorniy@proffiz.com>
2022-05-05 13:46:38 +0200

    fixed horizontal menu mobile scroll

--- packages/nfid-sdk-react/src/atoms/button/menu/index.tsx
+++ packages/nfid-sdk-react/src/atoms/button/menu/index.tsx
@@ -1,46 +1,44 @@
-import clsx from 'clsx';
-import React from 'react';
+import { Button, ButtonProps } from '..'
+import clsx from 'clsx'
+import React from 'react'
 
-import { Button, ButtonProps } from '..';
-
-export interface ButtonMenuProps
-  extends React.HTMLAttributes<HTMLDivElement> {
-  children: (toggle: () => void) => React.ReactNode;
-  buttonProps?: ButtonProps;
-  buttonElement?: React.ReactElement | string;
+export interface ButtonMenuProps extends React.HTMLAttributes<HTMLDivElement> {
+  children: (toggle: () => void) => React.ReactNode
+  buttonProps?: ButtonProps
+  buttonElement?: React.ReactElement | string
 }
 
 export const ButtonMenu: React.FC<ButtonMenuProps> = ({
   children,
   className,
   buttonProps,
-  buttonElement,
+  buttonElement
 }) => {
-  const [toggleMenu, setToggleMenu] = React.useState(false);
-  const ref = React.useRef<HTMLDivElement | null>(null);
+  const [toggleMenu, setToggleMenu] = React.useState(false)
+  const ref = React.useRef<HTMLDivElement | null>(null)
 
   const handleMenuToggle = React.useCallback(
     (e) => {
-      e.stopPropagation();
-      setToggleMenu(!toggleMenu);
+      e.stopPropagation()
+      setToggleMenu(!toggleMenu)
     },
     [toggleMenu]
-  );
+  )
 
   React.useEffect(() => {
     const handleClickOutside = (event: any) => {
       if (ref.current && !ref.current.contains(event.target)) {
-        setToggleMenu(false);
+        setToggleMenu(false)
       }
-    };
-    document.addEventListener('click', handleClickOutside, true);
+    }
+    document.addEventListener('click', handleClickOutside, true)
     return () => {
-      document.removeEventListener('click', handleClickOutside, true);
-    };
-  }, []);
+      document.removeEventListener('click', handleClickOutside, true)
+    }
+  }, [])
 
   return (
-    <div ref={ref}>
+    <div ref={ref} className={clsx("relative", toggleMenu ? '' : 'overflow-hidden')}>
       <Button
         {...buttonProps}
         onClick={(e) => handleMenuToggle(e)}
@@ -55,14 +53,14 @@ export const ButtonMenu: React.FC<ButtonMenuProps> = ({
 
       {toggleMenu && (
         <div
-          className="absolute top-0 left-0 z-[1] block w-full h-screen bg-black-base bg-opacity-25 overflow-hidden"
+          className='absolute top-0 left-0 z-[1] block w-full h-screen bg-black-base bg-opacity-25 overflow-hidden'
           onClick={() => setToggleMenu(false)}
         />
       )}
 
       <div
         className={clsx(
-          'z-10 h-screen text-base list-none bg-white shadow-md rounded absolute right-0 top-0 transition-transform ease-in duration-500',
+          'z-10 h-screen text-base list-none bg-white shadow-md rounded absolute right-0 top-0 transition-all ease-in duration-500',
           toggleMenu ? 'translate-x-0' : 'translate-x-full',
           className
         )}
@@ -70,5 +68,5 @@ export const ButtonMenu: React.FC<ButtonMenuProps> = ({
         {children(() => setToggleMenu(false))}
       </div>
     </div>
-  );
-};
+  )
+}
