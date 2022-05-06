e59793f1f1b888664b877686e9b3843d99c544c1
Pavlo Chorniy <pchorniy@proffiz.com>
2022-05-06 12:47:32 +0200

    chore([sc-1465]): accordion refactoring with animation

--- packages/nfid-sdk-react/src/molecules/accordion/index.stories.tsx
+++ packages/nfid-sdk-react/src/molecules/accordion/index.stories.tsx
@@ -14,7 +14,11 @@ const meta: Meta = {
 
 export default meta;
 
-const Template: Story<AccordionProps> = (args) => <Accordion {...args} />;
+const Template: Story<AccordionProps> = (args) => <div>
+  <Accordion {...args} />
+  <Accordion {...args} />
+  <Accordion {...args} />
+</div>;
 
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
@@ -22,5 +26,5 @@ export const Default = Template.bind({});
 
 Default.args = {
   title: "Example accordion title",
-  details: "Lorem ipsum"
+  details: "NFID is the most private, secure, and convenient way to sign in to third-party apps and websites using the devices you already own. When you see a Continue with NFID prompt on a participating app or website, it means you can set up an account using your NFID. No more usernames or passwords to manage, no more loss of privacy, and no more identity theft or theft of the cryptocurrency you’re holding."
 };
--- packages/nfid-sdk-react/src/molecules/accordion/index.tsx
+++ packages/nfid-sdk-react/src/molecules/accordion/index.tsx
@@ -1,66 +1,54 @@
-import clsx from 'clsx';
-import React from 'react';
+import React, { useRef, useState } from 'react'
 
-export interface AccordionProps
-  extends React.DetailedHTMLProps<
-    React.HTMLAttributes<HTMLDivElement>,
-    HTMLDivElement
-  > {
-  title: string;
-  details: string | React.ReactNode;
+export interface AccordionProps {
+  title: React.ReactNode
+  details: React.ReactNode
 }
 
-export const Accordion: React.FC<AccordionProps> = ({
-  className,
-  title,
-  details,
-}) => {
-  const [expanded, setExpanded] = React.useState(false);
+export const Accordion: React.FC<AccordionProps> = ({ title, details }) => {
+  const [active, setActive] = useState(false)
+  const [height, setHeight] = useState('0px')
+  const [rotate, setRotate] = useState('transform duration-700 ease')
+
+  const contentSpace = useRef<HTMLDivElement>(null)
+
+  function toggleAccordion() {
+    setActive((prevState) => !prevState)
+    setHeight(active ? '0px' : `${contentSpace.current && contentSpace.current.scrollHeight}px`)
+    setRotate(active ? 'transform duration-700 ease' : 'transform duration-700 ease rotate-180')
+  }
 
   return (
-    <div className={clsx('', className)}>
-      <div
-        className={clsx('w-full flex items-start pt-[10px] pb-[15px] sm:py-5')}
-        onClick={() => !expanded && setExpanded(!expanded)}
+    <div className="flex flex-col font-inter">
+      <button
+        className="py-6 box-border appearance-none cursor-pointer focus:outline-none flex items-center justify-between"
+        onClick={toggleAccordion}
       >
-        <div className="flex-1">
-          <div
-            className="font-bold cursor-pointer tracking-[0.01em] text-xl leading-[26px] hover:text-[#525457]"
-            onClick={() => setExpanded(!expanded)}
-          >
-            {title}
-          </div>
-          <div
-            className="mt-[18px] mb-[5px] text-base text-l sm:tracking-[0.01em] sm:text-lg sm:leading-[26px] sm:mt-5"
-            hidden={!expanded}
-          >
-            {details}
-          </div>
-        </div>
-        <div
-          className={clsx(
-            'ml-2 cursor-pointer lg:mx-4 transition-all',
-            expanded && 'rotate-180'
-          )}
-          onClick={() => setExpanded(!expanded)}
+        <p className="inline-block font-bold text-xl">{title}</p>
+        <svg
+          width='30'
+          height='30'
+          viewBox='0 0 30 30'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+          className={`${rotate} inline-block`}
         >
-          <svg
-            width="30"
-            height="30"
-            viewBox="0 0 30 30"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M8.34564 12L15 18.6543L21.6543 12"
-              stroke="black"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </div>
+          <path
+            d='M8.34564 12L15 18.6543L21.6543 12'
+            stroke='black'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          />
+        </svg>
+      </button>
+      <div
+        ref={contentSpace}
+        style={{ maxHeight: `${height}` }}
+        className="overflow-auto transition-max-height duration-700 ease-in-out"
+      >
+        <div className="pb-10 text-lg tracking-[0.01em] leading-[26px]">{details}</div>
       </div>
     </div>
-  );
-};
+  )
+}
