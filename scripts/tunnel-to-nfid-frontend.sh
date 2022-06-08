bf54998784fe4f9a8cbe51a60dac3a39fb0673f2
Philipp Litzenberger <philipp@indentitylabs.ooo>
2022-06-08 12:07:24 +0200

    feat(nfid): tunnel scripts

--- scripts/tunnel-to-nfid-frontend.sh
+++ scripts/tunnel-to-nfid-frontend.sh
@@ -4,6 +4,9 @@
 NGROK=./scripts/ngrok
 
 if [ -f "$NGROK" ]; then
+  # if [[ -z "${TUNNEL_DOMAIN}" ]]; then
+
+  # fi
   $NGROK http --region=${TUNNEL_REGION:-eu} --hostname=${TUNNEL_DOMAIN} ${TUNNEL_PORT}
 else
   echo "you need to download and unzip ngrok in this folder"
