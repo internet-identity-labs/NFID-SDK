071ff5003b5f0644c692c5e9c141c278fb1e751c
Philipp Litzenberger <philipp@indentitylabs.ooo>
2022-08-16 17:20:45 +0200

    chore: add missing variables

--- build/nfid/index.ts
+++ build/nfid/index.ts
@@ -6,6 +6,7 @@ declare const PUB_SUB_CHANNEL_CANISTER_ID: string;
 declare const USERGEEK_API_KEY: string;
 (window as any).FRONTEND_MODE = FRONTEND_MODE;
 (window as any).II_ENV = II_ENV;
+(window as any).IS_DEV = "true";
 (window as any).IC_HOST = window.location.origin;
 (window as any).INTERNET_IDENTITY_CANISTER_ID = INTERNET_IDENTITY_CANISTER_ID;
 (window as any).IDENTITY_MANAGER_CANISTER_ID = IDENTITY_MANAGER_CANISTER_ID;
@@ -15,3 +16,5 @@ declare const USERGEEK_API_KEY: string;
 (window as any).SENTRY_RELEASE = "NFID-SDK";
 (window as any).GOOGLE_CLIENT_ID =
   "339872286671-87oou3adnvl7hst9gd90r9k7j6enl7vk.apps.googleusercontent.com";
+(window as any).VERIFY_PHONE_NUMBER = "/verify";
+(window as any).SIGNIN_GOOGLE = "/signin";
