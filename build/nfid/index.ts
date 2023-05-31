66372cfa2ad3825038a4e007df544b2581470403
Philipp Litzenberger <philipp@identitylabs.ooo>
2023-05-31 12:32:05 +0200

    feat([sc-7291]): update frontend build

--- build/nfid/index.ts
+++ build/nfid/index.ts
@@ -3,16 +3,24 @@ declare const II_ENV: string;
 declare const INTERNET_IDENTITY_CANISTER_ID: string;
 declare const IDENTITY_MANAGER_CANISTER_ID: string;
 declare const PUB_SUB_CHANNEL_CANISTER_ID: string;
-declare const USERGEEK_API_KEY: string;
 (window as any).FRONTEND_MODE = FRONTEND_MODE;
 (window as any).II_ENV = II_ENV;
 (window as any).IS_DEV = "true";
+(window as any).IS_E2E_TEST = "false";
 (window as any).IC_HOST = window.location.origin;
 (window as any).INTERNET_IDENTITY_CANISTER_ID = INTERNET_IDENTITY_CANISTER_ID;
 (window as any).IDENTITY_MANAGER_CANISTER_ID = IDENTITY_MANAGER_CANISTER_ID;
 (window as any).PUB_SUB_CHANNEL_CANISTER_ID = PUB_SUB_CHANNEL_CANISTER_ID;
+(window as any).VAULT_CANISTER_ID = "ymvb6-7qaaa-aaaan-qbgga-cai";
+(window as any).ETH_SECRET_STORAGE_CANISTER_ID = "25nnx-kyaaa-aaaao-ab43a-cai";
+(window as any).ECDSA_SIGNER_CANISTER_ID = "txkre-oyaaa-aaaap-qa3za-cai";
+(window as any).BTC_SIGNER_CANISTER_ID = "7b5jg-paaaa-aaaap-aa32q-cai";
+
 (window as any).USERGEEK_API_KEY = "";
 (window as any).VERIFIER_CANISTER_ID = "5yjgb-gyaaa-aaaao-aabwa-cai";
+(window as any).LEDGER_CANISTER_ID = "ryjl3-tyaaa-aaaaa-aaaba-cai";
+(window as any).CYCLES_MINTER_CANISTER_ID = "rkp4c-7iaaa-aaaaa-aaaca-cai";
+(window as any).CURRCONV_TOKEN = "THIS IS A SECRET";
 (window as any).SENTRY_RELEASE = "NFID-SDK";
 (window as any).GOOGLE_CLIENT_ID =
   "339872286671-87oou3adnvl7hst9gd90r9k7j6enl7vk.apps.googleusercontent.com";
