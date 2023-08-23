4925108e198c3b755b2485e6400497c311855b49
Philipp Litzenberger <philipp@identitylabs.ooo>
2023-08-23 10:09:14 +0200

    feat([sc-7957]): update NFID build

---
+++ examples/create-ic-app-react-demo/canister/nfid_frontend/build/static/js/719.7c036e7f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(globalThis.webpackChunk=globalThis.webpackChunk||[]).push([[719],{17719:(a,l,t)=>{t.d(l,{CSs:()=>r});var e=t(99818);function r(a){return(0,e.w_)({tag:"svg",attr:{fill:"currentColor",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{fillRule:"evenodd",d:"M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"}}]})(a)}}}]);
+//# sourceMappingURL=719.7c036e7f.chunk.js.map
